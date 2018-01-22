6a62914802db038b71fd39c8e8c99a898c85576c
Allen Madsen <blatyo@gmail.com>
2018-01-22 10:40:52 -0500

    Add package meta

--- mix.exs
+++ mix.exs
@@ -6,9 +6,44 @@ defmodule PhoenixPubsubConduitAmqp.Mixfile do
       app: :phoenix_pubsub_conduit_amqp,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: "A phoenix pubsub adapter that uses conduit and AMQP",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+
+      # Docs
+      name: "PhoenixPubsubConduitAMQP",
+      source_url: "https://github.com/conduitframework/phoenix_pubsub_conduit_amqp",
+      homepage_url: "https://hexdocs.pm/phoenix_pubsub_conduit_amqp",
+      docs: docs(),
+
+      # Package
+      description: "AMQP adapter for Conduit.",
+      package: package(),
+
       aliases: ["publish": ["hex.publish", &git_tag/1]]
+    ]
+  end
+
+  defp package do
+    [
+      name: :phoenix_pubsub_conduit_amqp,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Allen Madsen"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/conduitframework/phoenix_pubsub_conduit_amqp",
+        "Docs" => "https://hexdocs.pm/phoenix_pubsub_conduit_amqp"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      project: "Phoenix.Pubsub.ConduitAMQP",
+      extra_section: "Guides",
+      extras: ["README.md"],
+      assets: ["assets"]
     ]
   end
 
@@ -24,7 +59,8 @@ defmodule PhoenixPubsubConduitAmqp.Mixfile do
     [
       {:phoenix_pubsub, "~> 1.0"},
       {:conduit_amqp, "~> 0.4.3"},
-      {:recon, "~> 2.3"}
+      {:recon, "~> 2.3"},
+      {:ex_doc, "~> 0.14", only: :dev}
     ]
   end
 
