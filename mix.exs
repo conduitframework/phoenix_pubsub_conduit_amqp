1fa2c9936bad45d7dd1c26d756239633220dafe0
Allen Madsen <blatyo@gmail.com>
2018-01-22 10:33:56 -0500

    Add publish task

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule PhoenixPubsubConduitAmqp.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: ["publish": ["hex.publish", &git_tag/1]]
     ]
   end
 
@@ -25,5 +26,11 @@ defmodule PhoenixPubsubConduitAmqp.Mixfile do
       {:conduit_amqp, "~> 0.4.3"},
       {:recon, "~> 2.3"}
     ]
+  end
+
+  defp git_tag(_args) do
+    tag = "v" <> Mix.Project.config[:version]
+    System.cmd("git", ["tag", tag])
+    System.cmd("git", ["push", "origin", tag])
   end
 end
